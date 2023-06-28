0559ca8cc61740d2f9d0cf14d83f1e6d0889ddb9
mexhi1 <137896463+mexhi1@users.noreply.github.com>
2023-06-28 22:56:47 +0200

    Add files via upload

---
+++ FinalProject/FinalProject/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  FinalProject
+//
+//  Created by user on 28.6.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+   @State private var welcomeText = "What is your name?"
+    @State private var name = ""
+    
+    var body: some View {
+        VStack {
+            
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
